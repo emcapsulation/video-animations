61c1674783f8958ce764814fec0bc77a2aa1cde1
emily-brooks <emilyalicebrooks@hotmail.co.uk>
2025-01-19 20:45:06 +1100

    Boyer Moore: Finished all animations

--- boyer-moore-majority/boyer-moore-majority.cpp
+++ boyer-moore-majority/boyer-moore-majority.cpp
@@ -1,19 +1,30 @@
-int majorityElement(vector<int>& nums) {
-    int count = 1;
-    int candidate = nums[0];
+// Boyer Moore's Majority Vote algorithm written in C++
+// Returns the majority element if there is one, or -1
+int boyerMoore(vector<int>& nums) {
+    int n = nums.size(), target = nums.size()/2+1;
+    int count = 1, candidate = nums[0];
 
-    for (size_t i = 1; i < nums.size(); i++) {           
+    for (int i = 1; i < n; i++) {           
         if (nums[i] == candidate) {
             count += 1;
         } else {
             count -= 1;
         }
-
         if (count == 0) {
             candidate = nums[i];
             count = 1;
         }
     }
 
-    return candidate;
+    int cur = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == candidate) {
+            cur += 1;
+        }
+        if (cur == target) {
+            return candidate;
+        }
+    }
+
+    return -1;
 }
