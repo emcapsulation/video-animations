34d63e5bdd76e4160780190059d7893d187d06f0
emily-brooks <emilyalicebrooks@hotmail.co.uk>
2025-02-17 19:25:00 +1100

    Baseball Elimination: Max flow min cut animations

--- baseball-elimination/be-anim.py
+++ baseball-elimination/be-anim.py
@@ -1,8 +1,10 @@
 from manim import *
 from Leaderboard import Board
+from FlowNetwork import FlowNetwork
 
 
 config.background_color = "#15131c"
+
 
 
 class Leaderboard(Scene):
@@ -110,4 +112,124 @@ class Leaderboard(Scene):
 			Transform(question_group, no_win_group)
 		)
 		self.wait(2)
+
+
+
+class BaseballIntro(Scene):
+	def construct(self):
+		Text.set_default(font="Monospace")
+
+		title_text = Text("Baseball Elimination Problem").shift(UP*3)
+		self.play(Write(title_text))
+
+		max_flow_text = Text("max flow application", font_size=24).shift(UP*2)       
+		self.play(Write(max_flow_text))
+
+		# [N1 -> N2, weight]
+		edge_list = [[0, 1, 10], [0, 2, 12], [0, 3, 4], [1, 4, 3], [1, 2, 6], [3, 5, 7], [2, 6, 9], [4, 6, 10], [5, 6, 5]]
+		positions = [[-5, 0], [-1, -0.5], [1, 2], [-2, -2], [1.5, -0.5], [3, -2], [5, 0]]
+		fn = FlowNetwork(edge_list, positions)
+		fn.create_network(0.9, DOWN*1)
+		self.wait(2)
+
+		src_node = Dot(color=TEAL, radius=0.2)
+		src_text = Text("source (s)", font_size=24)
+		src_group = VGroup(src_node, src_text).arrange()
+
+		sink_node = Dot(color=ORANGE, radius=0.2)
+		sink_text = Text("sink (t)", font_size=24)
+		sink_group = VGroup(sink_node, sink_text).arrange()
+
+		src_sink = VGroup(src_group, sink_group).arrange(buff=0.5).shift(UP*2)
+
+		max_flow_title = Text("Max Flow").shift(UP*3)
+		self.play(Transform(max_flow_text, src_sink), Transform(title_text, max_flow_title))
+
+		fn.bfs_animation(self)
+		self.wait(2)
+
+		question_text = Text("What is the maximum flow that can reach", font_size=24)
+		question_mark = Text("?", font_size=24)
+		question_group = VGroup(question_text, sink_node, question_mark).arrange(buff=0.5).shift(UP*2)
+		
+		self.play(Transform(max_flow_text, question_group))
+		self.wait(2)
+
+		self.play(FadeOut(fn.network))
+		self.wait(2)
+
+		edge_list = [[0, 1, 5], [1, 3, 3], [0, 2, 2], [2, 3, 4]]
+		positions = [[-3, 0], [0, 2], [0, -2], [3, 0]]
+		fn2 = FlowNetwork(edge_list, positions)
+		fn2.create_network(1, DOWN*1)
+		fn2.bfs_animation(self)
+		self.wait(2)
+
+		fn2.flow_animation(self)
+		self.wait(2)
+
+		max_flow_calc = Text("max flow = 3 + 2 = 5", font_size=24).shift(UP*2)
+		self.play(Transform(max_flow_text, max_flow_calc))
+		self.wait(2)
+
+
+
+class MaxFlowMinCut(Scene):
+	def construct(self):
+		Text.set_default(font="Monospace")
+
+		max_flow_title = Text("Max Flow").shift(UP*3)
+		self.add(max_flow_title)
+
+		mfmc_text = Text("max flow = min cut", color=WHITE, font_size=36)
+		mfmc_text[8:11].set_color(YELLOW)
+		mfmc_text[11:14].set_color(PINK)
+		self.play(Write(mfmc_text))
+		self.wait(2)
+		
+		cut_text = Text("set of edges whose removal\ndisconnects source from sink", color=PINK, font_size=24).move_to(mfmc_text.get_center() + UP*1.1 + RIGHT*2)
+		self.play(Write(cut_text))
+		self.wait(2)
+
+		min_text = Text("smallest total of\nedge weights", color=YELLOW, font_size=24).move_to(mfmc_text.get_center() + DOWN*1.1 + LEFT*0.5)
+		self.play(Write(min_text))
+		self.wait(2)
+
+		self.play(FadeOut(cut_text), FadeOut(min_text), mfmc_text.animate.scale(0.8).move_to(UP*2))
+		self.wait(2)
+
+
+		edge_list = [[0, 1, 5], [1, 3, 3], [0, 2, 2], [2, 3, 4]]
+		positions = [[-3, 0], [0, 2], [0, -2], [3, 0]]
+		fn2 = FlowNetwork(edge_list, positions)
+		fn2.create_network(0.9, DOWN*1.5)
+		fn2.bfs_animation(self)
+		self.wait(2)
+
+
+		fn2.remove_edges_put_back(self, [[0, 1], [2, 3]])
+		fn2.remove_edges_put_back(self, [[0, 1], [0, 2]])
+		fn2.remove_edges_put_back(self, [[0, 2], [1, 3]])
+		fn2.remove_edges_put_back(self, [[1, 3], [2, 3]])
+
+
+		fn2.min_cut_animation(self, [[0, 2], [1, 3]])
+		self.wait(2)
+
+		self.remove(fn2.network)
+		self.wait(2)
+
+
+		edge_list = [[0, 1, 10], [0, 2, 12], [0, 3, 4], [1, 4, 3], [1, 2, 6], [3, 5, 7], [2, 6, 9], [4, 6, 10], [5, 6, 5]]
+		positions = [[-5, 0], [-1, -0.5], [1, 2], [-2, -2], [1.5, -0.5], [3, -2], [5, 0]]
+		fn = FlowNetwork(edge_list, positions)
+		fn.create_network(0.9, DOWN*1.5)
+		fn.bfs_animation(self)
+		self.wait(2)
+
+		fn.min_cut_animation(self, [[0, 3], [1, 4], [2, 6]])
+		self.wait(2)
+
+
+
 
